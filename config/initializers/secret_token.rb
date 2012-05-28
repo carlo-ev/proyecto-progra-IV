c55b29d5d9149ee8ddafbe27c2556209dc3df95a
Saúl Eduaro Cabrera <scabrera92@hotmail.com>
2012-05-28 16:12:22 -0600

    created rails project/gems: therubyracer, twitter-bootstrap-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoProgra4::Application.config.secret_token = 'a862e103003006b6e8e0444c32d12bcaa9a5e5ba8fd2eb1dcb278de03ecd6ecf95399d56ffff15de7fdfbaa438b95ce629b2ce91d061b673f2af5ef991cbee48'
